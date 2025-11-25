4127cb834dd4e892b89be2489f3e9abdcda54d4c
Viquetaure2 <victor.desrosiers@hotmail.com>
2025-11-24 23:20:40 -0500

    fini badge

--- src/vue/BadgeRacinette.java
+++ src/vue/BadgeRacinette.java
@@ -6,66 +6,73 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 
 public class BadgeRacinette extends AnchorPane{
-	public enum COULEUR_DAUPHIN{BLEU, ROSE};
 	public enum COULEUR{ROUGE, BLEU, JAUNE, NOIR};
 
 	public BadgeRacinette() {
 		//AnchorPane this = new AnchorPane();
 		this.setPrefHeight(200);
 		this.setPrefWidth(166);
-		this.setStyle("-fx-background-color:red");
+		this.setStyle("-fx-background-color:blue");
 		
 		ImageView ecusson = new ImageView();
 		ecusson.setImage(new Image("vue/badge/armoirie.png"));
 		this.getChildren().add(ecusson);
 		
 		this.setVagues();
-		this.getChildren().add(vagues);
+		this.getChildren().add(dragon);
 		
-		this.setDauphinRose();
-		this.getChildren().add(dauphinRose);
+		this.setRacinetteDeux();
+		this.getChildren().add(racinetteDeux);
 		
-		this.setDauphinBleu(COULEUR_DAUPHIN.BLEU);
-		this.getChildren().add(dauphinBleu);
+		this.setRacinetteUn();
+		this.getChildren().add(racinetteUn);
 		
 		Label motto = new Label();
 		motto.setText("RACINETTE!!");
 		motto.setStyle("-fx-font:25px Tahoma; -fx-font-weight-bold;");
-		motto.setLayoutX(30);
-		motto.setLayoutY(190);
+		motto.setLayoutX(80);
+		motto.setLayoutY(50);
 		this.getChildren().add(motto);
 	}
 	
-	protected ImageView dauphinBleu = new ImageView();
-	public void setDauphinBleu(COULEUR_DAUPHIN couleur) {
-		dauphinBleu.setImage(new Image("vue/badge/dauphin-bleu.png"));
-		dauphinBleu.setLayoutX(160);
-		dauphinBleu.setLayoutY(80);
-		dauphinBleu.setScaleX(-1);
+	protected ImageView racinetteUn = new ImageView();
+	public void setRacinetteUn() {
+		racinetteUn.setImage(new Image("vue/badge/racinette.png"));
+		racinetteUn.setScaleX(-0.5);
+		racinetteUn.setScaleY(0.5);
+		racinetteUn.setLayoutX(175);
+		racinetteUn.setLayoutY(50);
 	}
 	
-	protected ImageView dauphinRose = new ImageView();
-	public void setDauphinRose() {
-		dauphinRose.setImage(new Image("vue/badge/dauphin-rose.png"));
-		dauphinRose.setLayoutX(40);
-		dauphinRose.setLayoutY(80);
+	protected ImageView racinetteDeux = new ImageView();
+	public void setRacinetteDeux() {
+		racinetteDeux.setImage(new Image("vue/badge/racinette.png"));
+		racinetteDeux.setScaleX(0.5);
+		racinetteDeux.setScaleY(0.5);
+		racinetteDeux.setLayoutX(55);
+		racinetteDeux.setLayoutY(50);
 	}
 	
-	protected ImageView vagues = new ImageView();
+	protected ImageView dragon = new ImageView();
 	public void setVagues() {
-		vagues.setImage(new Image("vue/badge/vagues-rondes.png"));
-		vagues.setScaleX(0.2);
-		vagues.setScaleY(0.2);
-		vagues.setLayoutX(54);
-		vagues.setLayoutY(240);
+		dragon.setImage(new Image("vue/badge/dragon.png"));
+		dragon.setLayoutX(70);
+		dragon.setLayoutY(180);
 	}
 	
 	public void setCouleur(COULEUR couleur) {
-		this.setStyle("-fx-background-color:red");
+		if (couleur == COULEUR.BLEU)
+			this.setStyle("-fx-background-color:blue");
+		else if (couleur == COULEUR.JAUNE)
+			this.setStyle("-fx-background-color:yellow");
+		else if (couleur == COULEUR.NOIR)
+			this.setStyle("-fx-background-color:black");
+		else if (couleur == COULEUR.ROUGE)
+			this.setStyle("-fx-background-color:red");
 	}
 	
 	public BadgeRacinette ALaRacinetteBrune() {
-		this.setDauphinBleu(COULEUR_DAUPHIN.ROSE);
+		setCouleur(COULEUR.JAUNE);
 		return this;
 	}
 }
